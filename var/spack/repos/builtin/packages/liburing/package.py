4cee3c5b092a14ac1ba38acb0eeb152d4695134f
Harmen Stoppels <me@harmenstoppels.nl>
2025-02-14 14:26:04 +0100

    liburing: bump

--- var/spack/repos/builtin/packages/liburing/package.py
+++ var/spack/repos/builtin/packages/liburing/package.py
@@ -19,10 +19,11 @@ class Liburing(AutotoolsPackage):
     license("LGPL-2.1-or-later OR MIT")
 
     version("master", branch="master")
+    version("2.9", sha256="897b1153b55543e8b92a5a3eb9b906537a5fedcf8afaf241f8b8787940c79f8d")
     version("2.3", sha256="60b367dbdc6f2b0418a6e0cd203ee0049d9d629a36706fcf91dfb9428bae23c8")
 
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")  # only for tests, built automatically.
 
-    conflicts("platform=darwin", msg="Only supported on linux")
-    conflicts("platform=windows", msg="Only supported on linux")
+    requires("platform=linux")
+
